1af5ca898082d63c045f4c4c9b8b21cc2d5575d4
Megianto Adi Saputra <72857078+MegiantoAS@users.noreply.github.com>
2022-05-22 23:14:29 +0700

    validation form

--- Capstones/app/src/main/java/com/dicoding/capstones/LoginActivity.kt
+++ Capstones/app/src/main/java/com/dicoding/capstones/LoginActivity.kt
@@ -1,25 +1,76 @@
 package com.dicoding.capstones
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.view.WindowInsets
 import android.view.WindowManager
+import androidx.core.content.ContextCompat
 import com.dicoding.capstones.databinding.ActivityLoginBinding
-import kotlinx.android.synthetic.main.nav.*
+import com.jakewharton.rxbinding2.widget.RxTextView
+import io.reactivex.Observable
+import io.reactivex.functions.Function3
 
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val emailStream = RxTextView.textChanges(binding.editEmail)
+            .skipInitialValue()
+            .map { email ->
+                !Patterns.EMAIL_ADDRESS.matcher(email).matches()
+            }
+        emailStream.subscribe {
+            showEmailExistAlert(it)
+        }
+
+        val passwordStream = RxTextView.textChanges(binding.editPass)
+            .skipInitialValue()
+            .map { password ->
+                password.length < 6
+            }
+        passwordStream.subscribe {
+            showPasswordMinimalAlert(it)
+        }
+
+        val invalidFieldsStream = Observable.combineLatest(
+            emailStream,
+            passwordStream,
+            passwordStream,
+            Function3 { emailInvalid: Boolean, passwordInvalid: Boolean, passwordConfirmationInvalid: Boolean ->
+                !emailInvalid && !passwordInvalid && !passwordConfirmationInvalid
+            })
+        invalidFieldsStream.subscribe { isValid ->
+            if (isValid) {
+                binding.button.isEnabled = true
+                binding.button.setBackgroundColor(ContextCompat.getColor(this, R.color.purple_200))
+            } else {
+                binding.button.isEnabled = false
+                binding.button.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray))
+            }
+        }
+
         setupView()
         setupAction()
     }
+
+//validation for form
+    private fun showEmailExistAlert(isNotValid: Boolean) {
+        binding.editEmail.error = if (isNotValid) getString(R.string.email_not_valid) else null
+    }
+    private fun showPasswordMinimalAlert(isNotValid: Boolean) {
+        binding.editPass.error = if (isNotValid) getString(R.string.password_not_valid) else null
+    }
+
 
     private fun setupView() {
         @Suppress("DEPRECATION")
@@ -44,6 +95,5 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
             startActivity(intent)
         }
-
     }
 }
--- Capstones/app/src/main/java/com/dicoding/capstones/RegisterActivity.kt
+++ Capstones/app/src/main/java/com/dicoding/capstones/RegisterActivity.kt
@@ -1,23 +1,110 @@
 package com.dicoding.capstones
+import io.reactivex.functions.Function3
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.view.WindowInsets
 import android.view.WindowManager
+import androidx.core.content.ContextCompat
 import com.dicoding.capstones.databinding.ActivityRegisterBinding
+import com.jakewharton.rxbinding2.widget.RxTextView
+import io.reactivex.Observable
+import io.reactivex.functions.Function4
 import kotlinx.android.synthetic.main.nav.*
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding : ActivityRegisterBinding
+
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        vadidationForm()
         setupView()
         setupAction()
+    }
+
+    @SuppressLint("CheckResult")
+    private fun vadidationForm(){
+        val emailStream = RxTextView.textChanges(binding.inputEmail)
+            .skipInitialValue()
+            .map { email ->
+                !Patterns.EMAIL_ADDRESS.matcher(email).matches()
+            }
+        emailStream.subscribe {
+            showEmailExistAlert(it)
+        }
+
+        val passwordStream = RxTextView.textChanges(binding.inputPassword)
+            .skipInitialValue()
+            .map { password ->
+                password.length < 6
+            }
+        passwordStream.subscribe {
+            showPasswordMinimalAlert(it)
+        }
+
+        val passwordConfirmationStream = Observable.merge(
+            RxTextView.textChanges(binding.inputPassword)
+                .map { password ->
+                    password.toString() != binding.inputPassConf.text.toString()
+                },
+            RxTextView.textChanges(binding.inputPassConf)
+                .map { confirmPassword ->
+                    confirmPassword.toString() != binding.inputPassword.text.toString()
+                }
+        )
+        passwordConfirmationStream.subscribe {
+            showPasswordConfirmationAlert(it)
+        }
+
+        val phoneStream = RxTextView.textChanges(binding.inputNumber)
+            .skipInitialValue()
+            .map { phone ->
+                !Patterns.PHONE.matcher(phone).matches()
+            }
+        phoneStream.subscribe {
+            showPhoneAlert(it)
+        }
+
+        val invalidFieldsStream = Observable.combineLatest(
+            emailStream,
+            passwordStream,
+            passwordConfirmationStream,
+            phoneStream,
+            Function4 { emailInvalid: Boolean, passwordInvalid: Boolean, passwordConfirmationInvalid: Boolean, phoneInvalid: Boolean ->
+                !emailInvalid && !passwordInvalid && !passwordConfirmationInvalid && !phoneInvalid
+            })
+        invalidFieldsStream.subscribe { isValid ->
+            if (isValid) {
+                binding.button.isEnabled = true
+                binding.button.setBackgroundColor(ContextCompat.getColor(this, R.color.purple_200))
+            } else {
+                binding.button.isEnabled = false
+                binding.button.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray))
+            }
+        }
+    }
+
+    private fun showEmailExistAlert(isNotValid: Boolean) {
+        binding.inputEmail.error = if (isNotValid) getString(R.string.email_not_valid) else null
+    }
+
+    private fun showPasswordMinimalAlert(isNotValid: Boolean) {
+        binding.inputPassword.error = if (isNotValid) getString(R.string.password_not_valid) else null
+    }
+
+    private fun showPasswordConfirmationAlert(isNotValid: Boolean) {
+        binding.inputPassConf.error = if (isNotValid) getString(R.string.password_not_same) else null
+    }
+    private fun showPhoneAlert(isNotValid: Boolean) {
+        binding.inputNumber.error = if (isNotValid) getString(R.string.password_not_same) else null
     }
 
     private fun setupView() {
@@ -35,7 +122,7 @@ class RegisterActivity : AppCompatActivity() {
 
     private fun setupAction(){
         binding.daftar.setOnClickListener {
-            val intent = Intent(this@RegisterActivity, RegisterActivity::class.java)
+            val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
             startActivity(intent)
         }
 
