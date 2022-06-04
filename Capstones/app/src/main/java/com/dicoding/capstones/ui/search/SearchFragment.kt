7bc7b4c610132823b51e4d76f2fb409a2a58611c
Megianto Adi Saputra <72857078+MegiantoAS@users.noreply.github.com>
2022-06-04 19:30:09 +0700

    Upload photo blm

--- Capstones/app/src/main/java/com/dicoding/capstones/ui/search/SearchFragment.kt
+++ Capstones/app/src/main/java/com/dicoding/capstones/ui/search/SearchFragment.kt
@@ -22,7 +22,7 @@ import com.dicoding.capstones.adapter.itemhome.ClassListAdapter
 import com.dicoding.capstones.adapter.itemhome.SearchListAdapter
 import com.dicoding.capstones.databinding.FragmentSearchBinding
 import com.dicoding.capstones.network.SearchDataItem
-import kotlinx.android.synthetic.main.fragment_search.*
+//import kotlinx.android.synthetic.main.fragment_search.*
 
 class SearchFragment : Fragment() {
 
@@ -53,61 +53,61 @@ class SearchFragment : Fragment() {
         return root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val searchManager = context?.getSystemService(Context.SEARCH_SERVICE) as SearchManager
-        searchViewModel.listUser.observe(viewLifecycleOwner) { users ->
-            setUser(users)
-            searchView.clearFocus()
-        }
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(requireActivity().componentName))
-        searchView.queryHint = resources.getString(R.string.search_hint)
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
-            androidx.appcompat.widget.SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(p0: String?): Boolean {
-                searchView.clearFocus()
-                searchUserData(p0!!)
-                return true
-            }
-            override fun onQueryTextChange(p0: String?): Boolean {
-                return false
-            }
-        })
-    }
-
-    private fun searchUserData(q: String){
-        searchViewModel.searchDataUser(q)
-        searchViewModel.listUser.observe(this) { users -> setUser(users) }
-        searchViewModel.isLoading.observe(this) { showLoading(it)}
-    }
-
-    private fun showLoading(isLoading: Boolean) {
-        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
-    }
-
-    private fun setUser(users: List<SearchDataItem>) {
-        val listUser = ArrayList<SearchDataItem>()
-        listUser.clear()
-        for (user in users) {
-            val us = SearchDataItem(
-                user.classId,
-                user.userName,
-                user.userGender,
-                user.userPhoto,
-                user.subjectName
-            )
-            listUser.add(us)
-        }
-        val adapter = SearchListAdapter(listUser)
-        binding.rvListGuru.adapter = adapter
-
-        val layoutManager = LinearLayoutManager(context)
-        binding.rvListGuru.layoutManager = layoutManager
-    }
-
-
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        val searchManager = context?.getSystemService(Context.SEARCH_SERVICE) as SearchManager
+//        searchViewModel.listUser.observe(viewLifecycleOwner) { users ->
+//            setUser(users)
+//            searchView.clearFocus()
+//        }
+//        searchView.setSearchableInfo(searchManager.getSearchableInfo(requireActivity().componentName))
+//        searchView.queryHint = resources.getString(R.string.search_hint)
+//        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+//            androidx.appcompat.widget.SearchView.OnQueryTextListener {
+//            override fun onQueryTextSubmit(p0: String?): Boolean {
+//                searchView.clearFocus()
+//                searchUserData(p0!!)
+//                return true
+//            }
+//            override fun onQueryTextChange(p0: String?): Boolean {
+//                return false
+//            }
+//        })
+//    }
+//
+//    private fun searchUserData(q: String){
+//        searchViewModel.searchDataUser(q)
+//        searchViewModel.listUser.observe(this) { users -> setUser(users) }
+//        searchViewModel.isLoading.observe(this) { showLoading(it)}
+//    }
+//
+//    private fun showLoading(isLoading: Boolean) {
+//        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+//    }
+//
+//    private fun setUser(users: List<SearchDataItem>) {
+//        val listUser = ArrayList<SearchDataItem>()
+//        listUser.clear()
+//        for (user in users) {
+//            val us = SearchDataItem(
+//                user.classId,
+//                user.userName,
+//                user.userGender,
+//                user.userPhoto,
+//                user.subjectName
+//            )
+//            listUser.add(us)
+//        }
+//        val adapter = SearchListAdapter(listUser)
+//        binding.rvListGuru.adapter = adapter
+//
+//        val layoutManager = LinearLayoutManager(context)
+//        binding.rvListGuru.layoutManager = layoutManager
+//    }
+//
+//
+//
+//    override fun onDestroyView() {
+//        super.onDestroyView()
+//        _binding = null
+//    }
 }
