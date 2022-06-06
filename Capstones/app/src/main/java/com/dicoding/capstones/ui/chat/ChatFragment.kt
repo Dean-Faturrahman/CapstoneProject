f70e0676bd6f23c5ff92c930ea174c118a60df63
Dean Ghifari Faturrahman <dean12fatur@gmail.com>
2022-06-06 21:39:59 +0700

    ++

--- Capstones/app/src/main/java/com/dicoding/capstones/ui/chat/ChatFragment.kt
+++ Capstones/app/src/main/java/com/dicoding/capstones/ui/chat/ChatFragment.kt
@@ -30,7 +30,7 @@ class ChatFragment : Fragment() {
 
         return binding.root
     }
-
+    
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
